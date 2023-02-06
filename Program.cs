0500ca450eb875db4bc08e9b1be99b22c1e2077e
peng jimmy <jimmy.peng2020@outlook.com>
2023-02-06 20:17:28 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using gts;
+using HslCommunication.Profinet.Siemens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace 反射与特性
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mc mc1 = new mc();
+            mc.GT_AxisOn(1);
+            mc.GT_AlarmOn(1);
+            SiemensS7Net simensS7Net = new SiemensS7Net(SiemensPLCS.S1500);
+
+        }
+    }
+}
